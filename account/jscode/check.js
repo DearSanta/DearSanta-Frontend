73f3f8d5e9ed83965a9b2baf2d6fdf3d48ae7259
rtgc0915 <rtgc0915@gmail.com>
2022-11-19 10:15:00 +0900

    password_중복 js 수정

--- account/jscode/check.js
+++ account/jscode/check.js
@@ -1,12 +1,33 @@
-$('.pw').focusout(function(){
-    let pass1 = $("#pwd").val();
-    let pass2 = $("#check_pwd").val();
+// $('.pw').focusout(function(){
+//     let pass1 = $("#pwd").val();
+//     let pass2 = $("#check_pwd").val();
     
-    if (pass1 != "" || pass2 != ""){
-        if (pass1 == pass2){
-            $("#checkPw").html('');    
-        } else {
-            $("#checkPw").html('비밀번호가 일치하지 않습니다.');
+//     if (pass1 != "" || pass2 != ""){
+//         if (pass1 == pass2){
+//             $("#checkPw").html('');    
+//         } else {
+//             $("#checkPw").html('비밀번호가 일치하지 않습니다.');
+//         }
+//     }
+// })
+
+$(function(){
+    $("#alert-success").hide();
+    $("#alert-danger").hide();
+    $("input").keyup(function(){
+        var pwd1=$("#pwd").val();
+        var pwd2=$("#check_pwd").val();
+        if(pwd1 != "" || pwd2 != ""){
+            if(pwd1 == pwd2){
+                $("#alert-success").show();
+                $("#alert-danger").hide();
+            }else{
+                $("#alert-success").hide();
+                $("#alert-danger").show();
+                $("input").removeAttr("focus");
+                $("input").css("border","red");
+                // $("#submit").attr("disabled", "disabled");
+            }    
         }
-    }
-})
+    });
+});
